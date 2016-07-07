3716288c59cbca5aebd36965d43e0c54f21625d8
maxim.manuylov <maxim.manuylov@gmail.com>
2016-07-07 20:15:56 +0300

    set docker registry in build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION="0.2"
+DOCKER_REGISTRY="docker.io"
 
 rm -rf bin
 
@@ -8,16 +9,16 @@ GOOS=darwin  GOARCH=amd64 go build -o bin/macos/jongleur
 GOOS=linux   GOARCH=amd64 go build -o bin/linux/jongleur
 GOOS=windows GOARCH=amd64 go build -o bin/windows/jongleur.exe
 
-tar czf bin/jongleur-$VERSION-macos.tar.gz --directory=bin/macos jongleur
-tar czf bin/jongleur-$VERSION-linux.tar.gz --directory=bin/linux jongleur
-zip     bin/jongleur-$VERSION-windows.zip -j bin/windows/jongleur.exe
+#tar czf bin/jongleur-$VERSION-macos.tar.gz --directory=bin/macos jongleur
+#tar czf bin/jongleur-$VERSION-linux.tar.gz --directory=bin/linux jongleur
+#zip     bin/jongleur-$VERSION-windows.zip -j bin/windows/jongleur.exe
 
 mkdir bin/docker
 cp Dockerfile bin/docker/Dockerfile
 cp bin/linux/jongleur bin/docker/jongleur
 
-docker build --no-cache -t "maxmanuylov/jongleur:$VERSION" bin/docker
-docker tag "maxmanuylov/jongleur:$VERSION" "maxmanuylov/jongleur:latest"
+docker build --no-cache -t "$DOCKER_REGISTRY/maxmanuylov/jongleur:$VERSION" bin/docker
+docker tag "$DOCKER_REGISTRY/maxmanuylov/jongleur:$VERSION" "$DOCKER_REGISTRY/maxmanuylov/jongleur:latest"
 
-#docker push "maxmanuylov/jongleur:$VERSION"
-#docker push "maxmanuylov/jongleur:latest"
+#docker push "$DOCKER_REGISTRY/maxmanuylov/jongleur:$VERSION"
+#docker push "$DOCKER_REGISTRY/maxmanuylov/jongleur:latest"
