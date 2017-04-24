55452fc16a5c285d7ce9c6286fc22f1b4e1350e8
maxim.manuylov <maxim.manuylov@gmail.com>
2017-04-24 14:49:28 +0300

    drop docker image

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM alpine
-RUN apk add --no-cache ca-certificates && update-ca-certificates
-COPY jongleur /opt/bin/jongleur
-ENTRYPOINT ["/opt/bin/jongleur"]
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 VERSION="v0.9"
-DOCKER_REGISTRY="docker.io"
 
 rm -rf bin
 
@@ -11,16 +10,6 @@ GOOS=darwin  GOARCH=amd64 go build -o bin/macos/jongleur
 GOOS=linux   GOARCH=amd64 go build -o bin/linux/jongleur
 GOOS=windows GOARCH=amd64 go build -o bin/windows/jongleur.exe
 
-#tar czf bin/jongleur-$VERSION-macos.tar.gz --directory=bin/macos jongleur
-#tar czf bin/jongleur-$VERSION-linux.tar.gz --directory=bin/linux jongleur
-#zip     bin/jongleur-$VERSION-windows.zip -j bin/windows/jongleur.exe
-
-mkdir bin/docker
-cp Dockerfile bin/docker/Dockerfile
-cp bin/linux/jongleur bin/docker/jongleur
-
-docker build --no-cache -t "$DOCKER_REGISTRY/maxmanuylov/jongleur:$VERSION" bin/docker
-docker tag "$DOCKER_REGISTRY/maxmanuylov/jongleur:$VERSION" "$DOCKER_REGISTRY/maxmanuylov/jongleur:latest"
-
-#docker push "$DOCKER_REGISTRY/maxmanuylov/jongleur:$VERSION"
-#docker push "$DOCKER_REGISTRY/maxmanuylov/jongleur:latest"
+tar czf bin/jongleur-$VERSION-macos.tar.gz --directory=bin/macos jongleur
+tar czf bin/jongleur-$VERSION-linux.tar.gz --directory=bin/linux jongleur
+zip     bin/jongleur-$VERSION-windows.zip -j bin/windows/jongleur.exe
