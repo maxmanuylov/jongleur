256d70303944aa617d69877871129bc64717186e
maxim.manuylov <maxim.manuylov@gmail.com>
2017-05-03 17:00:35 +0300

    make build incremental

--- build/build.sh
+++ build/build.sh
@@ -12,11 +12,25 @@ govendor sync
 
 rm -rf bin
 
+TEMP_GOPATH="$PWD/.go"
+TEMP_PROJECT_PARENT_DIR="$TEMP_GOPATH/src/github.com/maxmanuylov"
+TEMP_PROJECT_DIR="$TEMP_PROJECT_PARENT_DIR/jongleur"
+PKG_DIR="$PWD/.pkg"
+
+mkdir -p "$TEMP_PROJECT_PARENT_DIR"
+mkdir -p "$PKG_DIR"
+
+ln -s "$PKG_DIR" "$TEMP_GOPATH/pkg"
+ln -s "$PWD" "$TEMP_PROJECT_DIR"
+
+cd "$TEMP_PROJECT_DIR"
+
+export GOPATH="$TEMP_GOPATH"
 export GOARCH=amd64
 
-GOOS=darwin  go build -o bin/macos/jongleur
-GOOS=linux   go build -o bin/linux/jongleur
-GOOS=windows go build -o bin/windows/jongleur.exe
+GOOS=darwin  go build -i -o bin/macos/jongleur
+GOOS=linux   go build -i -o bin/linux/jongleur
+GOOS=windows go build -i -o bin/windows/jongleur.exe
 
 tar czf bin/jongleur-$VERSION-macos.tar.gz --directory=bin/macos jongleur
 tar czf bin/jongleur-$VERSION-linux.tar.gz --directory=bin/linux jongleur
@@ -28,5 +42,7 @@ REVISION=$REVISION
 EOF
 
 echo "##message{\"kind\":\"Status\",\"text\":\"OK\"}"
+
+rm -rf "$TEMP_GOPATH"
 
 cd "$OLD_WD"
